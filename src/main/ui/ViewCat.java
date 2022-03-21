d9579de6fc1c1c7d9be8e503d4761e5db7d41db0
xuanmaii <luongxuanmai2109@gmail.com>
2022-03-21 01:49:40 -0700

    add viewcat

---
+++ src/main/ui/ViewCat.java
@@ -0,0 +1,38 @@
+package ui;
+
+import javax.swing.*;
+
+//a class that represents gui of category viewing options
+public class ViewCat extends JFrame {
+    protected JFrame frame;
+    protected JMenuBar menu;
+    protected WorldApp myWorld;
+
+    //EFFECTS: construct object to lay out gui of view-world option
+    public ViewCat(WorldApp myWorld) {
+        this.myWorld = myWorld;
+
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+
+        menu = new JMenuBar();
+        JButton cartoon = new JButton("CARTOON");
+        buttonProperties(cartoon);
+
+        //frame.getRootPane().setDefaultButton(allWorld);
+        JButton book = new JButton("BOOK");
+        buttonProperties(book);
+        JButton movie = new JButton("MOVIE");
+        buttonProperties(movie);
+        JButton game = new JButton("GAME");
+        buttonProperties(game);
+
+        frame.setJMenuBar(menu);
+        frame.setVisible(true);
+    }
+
+    public void buttonProperties(JButton button) {
+        menu.add(button);
+    }
+}
